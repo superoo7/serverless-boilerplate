0b76d6330f55dea0d4d3b3f3546a95b4ba456991
Lai Weng Han <jlwhoo7@gmail.com>
2019-03-05 14:05:40 +0800

    add test

---
+++ __tests__/handler.test.ts
@@ -0,0 +1,16 @@
+import { hello } from "./../handler";
+
+describe("handler", () => {
+  it("hello function", async () => {
+    const event = "event";
+    const expected = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event
+      })
+    };
+    const actual = await hello(event, null, null);
+    expect(actual).toEqual(expected);
+  });
+});
